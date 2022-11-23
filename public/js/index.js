48e48431187440153e807068b936120760dd0b87
Marie Kalouskova <kalouma4@fit.cvut.cz>
2022-11-23 18:39:16 +0100

    fix refactoring

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,3 @@
 document.getElementById("navbar").innerHTML = ejs.views_includes_navbar({});
 document.getElementById("navbar").innerHTML = ejs.views_slide_culling({});
-start_slide_culling_2d_vs_3d();
+start_slide_culling();
