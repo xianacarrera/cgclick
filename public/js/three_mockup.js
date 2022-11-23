744fd3e4c18b93354939ede0c3f7cfd36777cb9c
Xiana Carrera <xiana.carrera.alonso@gmail.com>
2022-11-23 20:07:47 +0100

    Animation

--- public/js/three_mockup.js
+++ public/js/three_mockup.js
@@ -28,6 +28,10 @@ function main(){
 
     function animate(){
         requestAnimationFrame( animate );
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        
         renderer.render( scene, camera );
     }
 
