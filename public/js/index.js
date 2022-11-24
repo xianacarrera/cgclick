c0f62aab827a8da5ecf975d512ef706d0b036477
Marie Kalouskova <kalouma4@fit.cvut.cz>
2022-11-24 11:56:58 +0100

    adding depth test to culling

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
 document.getElementById("navbar").innerHTML = ejs.views_includes_navbar({});
-document.getElementById("slide").innerHTML = ejs.views_slide_culling({});
-start_slide_culling();
+let culling = true;
+let depth_test = true;
+document.getElementById("slide").innerHTML = ejs.views_slide_triangle_cube({culling, depth_test});
+start_slide_triangle_cube(culling, depth_test);
