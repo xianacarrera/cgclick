276ea4c8d1dbee0db4698489d9c3fa0fab8232f8
Xiana Carrera <xiana.carrera.alonso@gmail.com>
2022-11-24 23:29:35 +0100

    Three slide prototype

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-//document.getElementById("navbar").innerHTML = ejs.views_includes_navbar({});
+document.getElementById("navbar").innerHTML = ejs.views_slide_figures({});
 
 threeAPI.initScene();
 // Torus example
