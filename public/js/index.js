35a3aa2c4d1a326eb78f3fc3a4ece690834eab25
Jiří Heller <hellejir@cvut.cz>
2022-11-24 11:07:57 +0100

    Polish HTML and CSS a little

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,3 @@
 document.getElementById("navbar").innerHTML = ejs.views_includes_navbar({});
-document.getElementById("navbar").innerHTML = ejs.views_slide_culling({});
+document.getElementById("slide").innerHTML = ejs.views_slide_culling({});
 start_slide_culling();
