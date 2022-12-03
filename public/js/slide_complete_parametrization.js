0b17698387a9d87a62af90806d9e30ee3b1b435a
Xiana Carrera <xiana.carrera.alonso@gmail.com>
2022-12-03 21:26:28 +0100

    Line building

--- public/js/slide_complete_parametrization.js
+++ public/js/slide_complete_parametrization.js
@@ -12,52 +12,50 @@ function addCompleteParametrizationListeners(){
 
         displayErrorMessage(false);
         const formData = new FormData(e.target);
-        //try {
-            console.log(formData.get("x"));
-            console.log(latex_to_js(formData.get("x")));
-
-            console.log(formData.get("x"));
-            //console.log(js_code);
+        try {
             buildCustomFunction(formData);
             threeAPI.createParametricGeometry(buildCustomFunction(formData));
             threeAPI.animate();
-            /*
         } catch (e) {
             displayErrorMessage(true);
             threeAPI.clear();
-        }*/
+        }
     });
 }
 
 function buildLine(symbol, input, constant){
-    console.log(spansText)
     let line = `${spansText[symbol][0] || ""} ${input || ""}`;        // Handle the case where there are no spans and/or no input
     for (let i = 1; i < spansText[symbol].length; i++){
-        console.log("???" + spansText[symbol][i])
         line += ` ${spansText[symbol][i]}`;
     }
     if (constant) line = line.replaceAll(constant.constSymbol, constant.constVal);
     line = line.replace("\\(", "");
     line = line.replace("\\)", "");
-    console.log(line);
-    return latex_to_js(line);
+    return line;
 }
 
 function buildCustomFunction(formData){
     let constant;
     if (formData.get("const-symbol")) constant = {constSymbol: formData.get("const-symbol"), constVal: formData.get("const-value")};
-    console.log("constant = " + constant.constSymbol + " " + constant.constVal);
-    let x = buildLine("x", formData.get("x"), constant);             // Build the x function
-    let y = buildLine("y", formData.get("y"), constant);             // Build the y function
-    let z = buildLine("z", formData.get("z"), constant);             // Build the z function
-    
-    console.log("x = " + x);
-    console.log("y = " + y);
-    console.log("z = " + z);
 
+    const umin = parseFloat(formData.get("u-min"));
+    const umax = parseFloat(formData.get("u-max"));
+    const vmin = parseFloat(formData.get("v-min"));
+    const vmax = parseFloat(formData.get("v-max"));
+    
     return function (u, v, target) {
-        u *= (formData.get("u-max") - formData.get("u-min")) + formData.get("u-min");           // Translate and scale u to [u_min, u_max]
-        v *= (formData.get("v-max") - formData.get("v-min")) + formData.get("v-min");           // Translate and scale v to [v_min, v_max]
+        u *= (umax - umin) + umin;           // Translate and scale u to [u_min, u_max]
+        v *= (vmax - vmin) + vmin;           // Translate and scale v to [v_min, v_max]
+
+        let x = buildLine("x", formData.get("x"), constant);             // Build the x function
+        let y = buildLine("y", formData.get("y"), constant);             // Build the y function
+        let z = buildLine("z", formData.get("z"), constant);             // Build the z function    
+
+        [x, y, z].forEach((symbol) => {
+            symbol = symbol.replaceAll("u", u);
+            symbol = symbol.replaceAll("v", v);
+            symbol = math.evaluate(latex_to_js(symbol));
+        });
 
         target.set(x, y, z);
     };
