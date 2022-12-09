76f48713b1cf168b31323fad89fedbd505ac2447
Xiana Carrera <xiana.carrera.alonso@gmail.com>
2022-12-09 17:28:28 +0100

    charts init

--- public/js/slide_image_parametrization.js
+++ public/js/slide_image_parametrization.js
@@ -2,6 +2,16 @@ let image_parameters_answers = { alpha: {}, beta_gamma: {} };
 let memory_students = {};
 let target_alpha, target_beta_gamma;
 
+let x_values_alpha;
+let y_values_alpha;
+let bar_colors_alpha;
+
+
+let x_values_beta_gamma;
+let y_values_beta_gamma;
+let bar_colors_beta_gamma;
+
+
 function displaySlideImageParameters(params) {
     document.getElementById("content").className = cardClasses;
     params.showButtons = true;
@@ -14,15 +24,19 @@ function displaySlideImageParameters(params) {
     MathJax.typeset();
 
     if (isTeacher){
+        x_values_alpha = [];
+        x_values_beta_gamma = [];
         target_alpha = params.target_alpha;
         target_beta_gamma = params.target_beta / params.target_gamma;
         for (let i = params.alpha.startIndex; i <= params.alpha.endIndex; i += params.alpha.step) {
             image_parameters_answers.alpha[i] = 0;
+            x_values_alpha.push(i);
         };
 
         for (let j = params.beta.startIndex; j <= params.beta.endIndex; j += params.beta.step) {
             for (let k = params.gamma.startIndex; k <= params.gamma.endIndex; k += params.gamma.step) {
                 image_parameters_answers.beta_gamma[j / k] = 0;
+                x_values_beta_gamma.push(i);
             }
         }
         
@@ -44,6 +58,46 @@ function displaySlideImageParameters(params) {
         { gamma: params.target_gamma, tone_mapping_alpha: params.target_alpha, tone_mapping_beta: params.target_beta }
     );
 }
+
+function drawCharts(){
+    // Discrete cuantitative variables -> bar charts
+    y_values_alpha = [];
+    x_values_alpha.forEach(x => {
+        y_values_alpha.push(image_parameters_answers.alpha[x]);
+    })
+    bar_colors_alpha = generateRandomColors(x_values_alpha.length);
+    y_values_beta_gamma = [];
+    x_values_beta_gamma.forEach(bg => {
+        y_values_beta_gamma.push(image_parameters_answers.beta_gamma[bg]);
+    })
+    bar_colors_beta_gamma.generateRandomColors(x_values_beta_gamma.length);
+
+    new Chart("myChart", {
+    type: "bar",
+    data: {
+        labels: x_values_alpha,
+        datasets: [{
+        backgroundColor: bar_colors_alpha,
+        data: y_values_alpha
+        }]
+    },
+    options: {...}
+    });
+}
+
+function generateRandomColors(n){
+    let colors = [];
+    while (colors.length < n) {
+        colors.push(`rgb(${rand(0, 255)}, ${rand(0, 255)}, ${rand(0, 255)})`);
+    }
+    return colors;
+}
+
+function rand(from, to) {
+    // ~~ is a faster Math.floor() for positive numbers -> double NOT bitwise operator
+    return ~~(Math.random() * (to - from)) + from;
+}
+
 
 function updateImageParametersGraphs(){
     let total = Object.values(image_parameters_answers.alpha).reduce((a, b) => a + b, 0);
